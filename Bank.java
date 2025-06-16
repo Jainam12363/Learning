fb80e727fb18a8ebb1cb2ab456023059a4a0134e
Jainam12363 <jainamk62@gmail.com>
2025-06-16 21:08:27 +0530

    Added Encapsulation practice Java programs

---
+++ Bank.java
@@ -0,0 +1,12 @@
+package OOPSpracticeEncapsulation;
+
+public class Bank {
+	private float amount;
+	public Bank(float amount){
+		this.amount=amount;
+	}
+	public float getMoney(){
+		return amount;
+	}
+
+}
---
+++ BankApp.java
@@ -0,0 +1,12 @@
+package OOPSpracticeEncapsulation;
+
+public class BankApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bank b=new Bank(10000);
+		System.out.println(b.getMoney()); 
+
+	}
+
+}
